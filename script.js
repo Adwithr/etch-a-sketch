3265c442f7f67c10912a65e73d10003cb4213f26
Adwithr <adwithr8277@gmail.com>
2024-01-31 00:18:46 +0530

    Completed resize functionality rainbow and black button remaining to debug

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
+let color = "black";
+
+document.addEventListener("DOMContentLoaded", () => {
+  createGridCells(16);
+});
+
 const GRIDSIZE = 500;
-let squaresPerSide = 16;
 
 let sketchArea = document.querySelector(".sketchArea");
 sketchArea.style.width = `${GRIDSIZE}px`;
 sketchArea.style.height = `${GRIDSIZE}px`;
 
-function createGridCells() {
+const reSize = document.querySelector("#resize");
+reSize.addEventListener("click", () => {
+  removeGridCells();
+  let input = prompt("Enter the grid size");
+  if (input <= 0 || input > 100) {
+    alert("Please enter something in the range 1-100");
+    createGridCells(16);
+  } else createGridCells(input);
+});
+
+const blackbtn = document.querySelector("#black");
+blackbtn.addEventListener("click", changeColor("black"));
+
+const rainbowbtn = document.querySelector("#rainbow");
+rainbowbtn.addEventListener("click", changeColor("random"));
+
+function createGridCells(squaresPerSide) {
   const numOfSquares = squaresPerSide * squaresPerSide;
   const btn = document.querySelector("#clear");
   for (let i = 0; i < numOfSquares; i++) {
-    const gridCell = document.createElement("div");
+    let gridCell = document.createElement("div");
     gridCell.style.width = `${GRIDSIZE / squaresPerSide - 2}px`;
     gridCell.style.height = `${GRIDSIZE / squaresPerSide - 2}px`;
     gridCell.classList.add("cells");
     sketchArea.appendChild(gridCell);
-    gridCell.addEventListener("mouseover", () => {
-      gridCell.style.backgroundColor = "black";
-    });
+    gridCell.addEventListener("mouseover", colorDiv);
     btn.addEventListener("click", () => {
       gridCell.style.backgroundColor = "white";
     });
   }
 }
 
-createGridCells();
+function removeGridCells() {
+  while (sketchArea.firstChild) {
+    sketchArea.removeChild(sketchArea.firstChild);
+  }
+}
+
+function colorDiv() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360},100%,50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function changeColor(colorChoice) {
+  color = colorChoice;
+}
